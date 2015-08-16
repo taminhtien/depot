1bf94b539b0ba68db6f12d2a7bc337da11331263
Tien Ta <taminhtien1993@gmail.com>
2015-08-16 12:51:54 +0700

    Add association among cart, line_item, product models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ActiveRecord::Base
+	before_destroy :ensure_not_referenced_by_any_line_item
+	has_many :line_items
+
 	validates :title, :description, :image_url, :price, presence: true
 	validates :price, numericality: { greater_than_or_equal_to: 0.01 }
 	validates :title, uniqueness: true
@@ -6,7 +9,21 @@ class Product < ActiveRecord::Base
 		with: %r{\.(gif|jpg|png)\Z}i,
 		message: 'must be a URL for GIF, JPG or PNG image.'
 	}
+	
 	def self.latest
 		Product.order(:updated_at).last
 	end
+
+	private
+		
+		# Ensures that there are no line items referencing this product
+		def ensure_not_referenced_by_any_line_item
+			if line_items.empty?
+				return true
+			else
+				errors.add(:base, 'Line items present')
+				return false # the row will not be destroyed
+			end
+		end
+	
 end
