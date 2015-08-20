b8a44058dab0bdceb926e207a63a8a6a794dee80
Tien Ta <taminhtien1993@gmail.com>
2015-08-20 18:16:02 +0700

    Edit 2 test in cart model test

--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -4,22 +4,33 @@ class CartTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
-  test "cart should have unique products" do
+
+  test "cart line items should contain unique products" do
   	ruby = products(:ruby)
   	rails = products(:rails)
   	cart = carts(:cart1)
-  	cart.add_product(ruby, ruby.price)
-  	cart.add_product(rails, rails.price)
-  	assert_equal 2, cart.line_items.size
-  	assert_equal 84.45, cart.total_price
+
+  	[ruby, rails].each do |product|
+      cart.add_product(product.id, product.price).save!
+    end
+    
+    # Gets the quantity of ruby product  
+    ruby_item = cart.line_items.find_by_product_id ruby.id
+    # The quantity of ruby product must be 1
+    assert_equal 1, ruby_item.quantity, "Cart line item is incorrect"
 	end
 
-	test "cart should have duplicate products" do
+	test "cart line items should contain duplicate products" do
   	ruby = products(:ruby)
   	cart = carts(:cart2)
-  	cart.add_product(ruby, ruby.price)
-  	cart.add_product(ruby, ruby.price)
-  	assert_equal 1, cart.line_items.size
-  	assert_equal 991, cart.total_price
+
+    3.times do
+      cart.add_product(ruby.id, ruby.price).save!
+    end
+  	
+    # Gets the quantity of ruby product  
+    ruby_item = cart.line_items.find_by_product_id ruby.id
+    # The quantity of ruby product must be 3
+    assert_equal 3, ruby_item.quantity, "Cart line item is incorrect"
 	end
 end
