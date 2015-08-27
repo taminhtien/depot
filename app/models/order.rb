cebb9c8b9b3a9e9b566cdb0fd6f7f11e4fa4b9de
Tien Ta <taminhtien1993@gmail.com>
2015-08-28 00:58:55 +0700

    Fix typo dependent

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,8 @@
 class Order < ActiveRecord::Base
-	has_many :line_items, dependant: :destroy
+	has_many :line_items, dependent: :destroy
 	PAYMENT_TYPES = [ "Check", "Credit card", "Purchase order" ]
 	validates :name, :address, :email, presence: true
-	validates :pay_type, inclusion: PAYMENT_TYPES, message: 'abc'
+	validates :pay_type, inclusion: PAYMENT_TYPES
 
 	def add_line_items_from_cart(cart)
 		cart.line_items.each do |item|
