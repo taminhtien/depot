54379176c7af9c18aa9e9e7363ed50d027bcbbb1
Tien Ta <taminhtien1993@gmail.com>
2015-09-15 23:47:17 +0700

    Edit the content of received email

--- app/mailers/order_notifier.rb
+++ app/mailers/order_notifier.rb
@@ -1,12 +1,12 @@
 class OrderNotifier < ApplicationMailer
-
+  default from: 'Sam Ruby <depot@example.com>'
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.order_notifier.received.subject
   #
-  def received
-    @greeting = "Hi"
+  def received(order)
+    @order = order
 
     mail to: "to@example.org"
   end
